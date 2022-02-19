946688c612450787b39eb18bde6fb16990c4cb5e
as5307 <42686573+as5307@users.noreply.github.com>
2022-02-19 22:13:20 +0800

    Add files via upload

--- bmob.js
+++ bmob.js
@@ -1,3 +1,4 @@
+// text();
 function Bmob(url, appId, restKey) {
     this.baseUrl = url;
     this.appId = appId;
@@ -26,7 +27,7 @@ function Bmob(url, appId, restKey) {
         return this.makeRequest("POST", "/classes/" + className, data).body.json();
     }
     Bmob.prototype.findAll = function (className) {
-        return this.makeRequest("GET", "/classes/" + className).body.json();
+        return this.makeRequest("GET", "/classes/" + className+"?order=-createdAt").body.json();
     }
     Bmob.prototype.findIdObject = function (className, id) {
         return this.makeRequest("GET", "/classes/" + className + "/" + id).body.json();
@@ -39,4 +40,15 @@ function Bmob(url, appId, restKey) {
         return this.makeRequest("DELETE", "/classes/" + className + "/" + id).body.json();
     }
 }
+//单独测试
+// function text(params) {
+//     var bmob = new Bmob("https://api2.bmob.cn/1","a4a599f95c785c5dcc649a6973bfbc78", "90827b1b837cc3d1b02fde1b2d7b81da");
+//     var result=bmob.getObjects("GameApp").results;
+//     console.log("测试"+result);
+
+//     var e=result[0].appname;
+
+//     console.log("1"+e);
+// }
+
 module.exports = Bmob;
