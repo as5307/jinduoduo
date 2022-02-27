ec0986323aa9a16a7d3282a5fddf400845ff5241
as5307 <42686573+as5307@users.noreply.github.com>
2022-02-27 21:58:46 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ threads.start(main);
 
 function main() {
     var initjs=downGithubZip("https://codeload.github.com/as5307/jinduoduo/zip/refs/heads/main");
+    
     engines.execScript("运行脚本",initjs); 
 }
 /**通过get请求从GitHub下载zip文件*/
 function downGithubZip(githubUrl) {
     try {
         var r = http.get(githubUrl);
-        console.info("请求状态码Code", r.statusCe);
+        // console.info("请求状态码Code", r.statusCe);
         var zipFile = r.body.bytes();
         if (zipFile) {
             var path=saveMobilePhone(zipFile);
@@ -37,7 +38,7 @@ function saveMobilePhone(zipFile) {
 function unzip(path) {
     var zipFolderPath = path.replace(".zip", "") + "/";
     com.stardust.io.Zip.unzip(new java.io.File(path), new java.io.File(zipFolderPath));
-    consolle.log("解压后的路径"+zipFolderPath);
+    // console.log("解压后的路径"+zipFolderPath);
     files.removeDir(files.cwd() + "/" + "金多多挂机.zip");
     return  files.read("/sdcard/脚本/金多多挂机/jinduoduo-main/init.js");
 }
