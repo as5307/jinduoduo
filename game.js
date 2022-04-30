326227a69d85aa7c3399c494f0cc18617e5b32bb
as5307 <42686573+as5307@users.noreply.github.com>
2022-04-30 12:58:58 +0800

    Add files via upload

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
-function Game(appName, packageName,color,isClickable,runTime) {
+function Game(appName, packageName,color,isClickable,runTime,url) {
     this.appName = appName;
     this.packageName = packageName;
     this.color = color;
     this.isClickable = isClickable;
     this.runTime = runTime;
+    this.url = url;
 }
 module.exports=Game;
